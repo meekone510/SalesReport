a7b427e1b1befcd5db4221abc5cd4fe3ebe9f1c1
Kun Bao <kun.bao@sap.com>
2016-01-17 00:35:15 -0800

    change to 43M

--- WebContent/salesreport/salesReport.controller.js
+++ WebContent/salesreport/salesReport.controller.js
@@ -58,7 +58,7 @@ sap.ui.controller(
 										"sales" : "20M"
 									}, {
 										"quarter" : "Q2",
-										"sales" : "21M"
+										"sales" : "43M"
 									}, {
 										"quarter" : "Q3",
 										"sales" : "12M"
